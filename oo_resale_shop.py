ace66ea1b5fc80ad21def7d7fe1cafbbfe500a49
Mack Case <hcase@smith.edu>
2024-02-04 19:29:53 -0500

    added method to ResaleShop

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,25 @@
 class ResaleShop:
+    from typing import Dict, Optional
+    from computer import Computer
 
+    inventory : Dict[int, Dict] = {}
     # What attributes will it need?
+    inventory = [] # Computer objects will go in here
+    
+    itemID = 0
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, inventory):
+        self.inventory = inventory
+
+    def buy(self, inventory, computer):
+        global itemID
+        itemID += 1
+        inventory[itemID] = computer
+        return itemID #not returning atm
 
     # What methods will you need?
+
+def main():
+    ResaleShop.buy()
