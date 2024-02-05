6fb62d579fa3785905fb140b74f2ee0f718a2067
Mack Case <hcase@smith.edu>
2024-02-05 17:30:20 -0500

    fixed a couple things in oo_resale_shop; calling Computer in oo is still confused

--- computer.py
+++ computer.py
@@ -1,6 +1,6 @@
 class Computer:
 
-    #itemID: int
+    # What attributes will it need?
 
     description: str
     processor_type: str
@@ -9,14 +9,12 @@ class Computer:
     operating_system: str
     year_made: int
     price: float
-    itemID: int
-
-    # What attributes will it need?
+    #itemID: int
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__(self, description, processor, hard_drive, memory, os, year, price):
-        self.decription = description
+        self.description = description
         self.processor_type = processor
         self.hard_drive_capacity = hard_drive
         self.memory = memory
@@ -25,29 +23,22 @@ class Computer:
         self.price = price
         
 
-    def create_computer (self, description, processor, hard_drive, memory, os, year, price):
-        self.decription = description
-        self.processor_type = processor
-        self.hard_drive_capacity = hard_drive
-        self.memory = memory
-        self.operating_system = os
-        self.year_made = year
-        self.price = price
-        return {'description': description,
-            'processor_type': processor,
-            'hard_drive_capacity': hard_drive,
-            'memory': memory,
-            'operating_system': os,
-            'year_made': year,
-            'price': price}
+    def create_computer (self):
+        return {'description': self.description,
+            'processor_type': self.processor_type,
+            'hard_drive_capacity': self.hard_drive_capacity,
+            'memory': self.memory,
+            'operating_system': self.operating_system,
+            'year_made': self.year_made,
+            'price': self.price}
+        #print(self.description, processor, hard_drive, memory, os, year, price)
     
    
 
 def main():
     computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
-    print(computer)
-    #computer = Computer.create_computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
-        
+    computer.create_computer()
+    
         
 
 if __name__ == "__main__":
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -4,22 +4,32 @@ class ResaleShop:
 
     inventory : Dict[int, Dict] = {}
     # What attributes will it need?
-    inventory = [] # Computer objects will go in here
     
     itemID = 0
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self, inventory):
-        self.inventory = inventory
+    def __init__(self):
+        self.inventory = []
+    
 
-    def buy(self, inventory, computer):
+    def buy(self, computer):
         global itemID
-        itemID += 1
-        inventory[itemID] = computer
-        return itemID #not returning atm
+        ResaleShop.itemID += 1
+        ResaleShop.inventory[ResaleShop.itemID] = computer
+        return ResaleShop.itemID #not returning atm
 
     # What methods will you need?
 
 def main():
-    ResaleShop.buy()
+    from computer import Computer
+    computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
+    computer = Computer.create_computer() #not understanding Computer as a class
+
+    print(computer)
+    #computer.create_computer()
+    #ResaleShop.buy(computer)
+
+if __name__ == "__main__":
+    main()
+    
