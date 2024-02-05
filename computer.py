7e065a13f5b7bb968d22522e5b0c1c05db1c7b47
Mack Case <hcase@smith.edu>
2024-02-04 19:23:38 -0500

    added method to Computer class

--- computer.py
+++ computer.py
@@ -9,6 +9,7 @@ class Computer:
     operating_system: str
     year_made: int
     price: float
+    itemID: int
 
     # What attributes will it need?
 
@@ -22,7 +23,32 @@ class Computer:
         self.operating_system = os
         self.year_made = year
         self.price = price
+        
 
-    # What methods will you need?
-        #def create_computer(self, description, processor, hard_drive, memory, os, year, price):
-        #actually maybe this belongs in main()
+    def create_computer (self, description, processor, hard_drive, memory, os, year, price):
+        self.decription = description
+        self.processor_type = processor
+        self.hard_drive_capacity = hard_drive
+        self.memory = memory
+        self.operating_system = os
+        self.year_made = year
+        self.price = price
+        return {'description': description,
+            'processor_type': processor,
+            'hard_drive_capacity': hard_drive,
+            'memory': memory,
+            'operating_system': os,
+            'year_made': year,
+            'price': price}
+    
+   
+
+def main():
+    computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
+    print(computer)
+    #computer = Computer.create_computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
+        
+        
+
+if __name__ == "__main__":
+        main()
