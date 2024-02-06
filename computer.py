2ef804d95fa8b1c3fae99a104b5a681156371422
Mack Case <hcase@smith.edu>
2024-02-06 10:05:09 -0500

    added dict to computer object

--- computer.py
+++ computer.py
@@ -9,7 +9,8 @@ class Computer:
     operating_system: str
     year_made: int
     price: float
-    #itemID: int
+    itemID: int
+    dictionary: dict
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
@@ -21,25 +22,34 @@ class Computer:
         self.operating_system = os
         self.year_made = year
         self.price = price
+        self.itemID = 0
+        self.dictionary = {'description':self.description, 
+                            'processor_type': self.processor_type,
+                            'hard_drive_capacity': self.hard_drive_capacity,
+                            'memory': self.memory,
+                            'operating_system': self.operating_system,
+                            'year_made': self.year_made,
+                            'price': self.price}
+
+
+    # def return_computer(self):
+    #     return {'description': self.description,
+    #         'processor_type': self.processor_type,
+    #         'hard_drive_capacity': self.hard_drive_capacity,
+    #         'memory': self.memory,
+    #         'operating_system': self.operating_system,
+    #         'year_made': self.year_made,
+    #         'price': self.price}
         
 
-    def create_computer (self):
-        return {'description': self.description,
-            'processor_type': self.processor_type,
-            'hard_drive_capacity': self.hard_drive_capacity,
-            'memory': self.memory,
-            'operating_system': self.operating_system,
-            'year_made': self.year_made,
-            'price': self.price}
-        #print(self.description, processor, hard_drive, memory, os, year, price)
     
    
 
-def main():
-    computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
-    computer.create_computer()
+# def main():
+#     computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
+#     computer.create_computer()
     
         
 
-if __name__ == "__main__":
-        main()
+# if __name__ == "__main__":
+#         main()
